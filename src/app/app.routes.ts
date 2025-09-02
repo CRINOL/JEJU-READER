f94b18c6935193a5b1550964001755e95347539a
CRINOL <crusal2012@gmail.com>
2025-09-02 16:03:23 +0300

    Jeju is on fire!

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },  {
+    path: 'scanner',
+    loadComponent: () => import('./scanner/scanner.page').then( m => m.ScannerPage)
+  }
+
+];
