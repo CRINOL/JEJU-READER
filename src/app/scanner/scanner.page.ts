f8e0938f401dbee80f3034228a7ccce72b44d9f3
CRINOL <crusal2012@gmail.com>
2025-09-03 18:48:05 +0300

    Jeju is live

--- src/app/scanner/scanner.page.ts
+++ src/app/scanner/scanner.page.ts
@@ -4,22 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { IonCol, IonContent, IonGrid, IonHeader, IonRow, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { BarcodeFormat } from '@zxing/library';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.page.html',
   styleUrls: ['./scanner.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonGrid, IonRow, IonCol, ZXingScannerModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonGrid, IonRow, IonCol, ZXingScannerModule, MatProgressBarModule, MatCardModule
+
+  ]
 })
 export class ScannerPage implements OnInit {
 
   title = "Event X"
-  tickets_total: number = 0
-  processed_total: number = 0;
+  tickets_total: number = 10000;
+  processed_total: number = 1000;
   failed_total: number = 0;
   conn_stat = "Disconnected"
   dev_vrf = "N/A"
+  scannedResult = ""
   
   allowedFormats = [BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX /*, ...*/];
 
@@ -28,4 +33,11 @@ export class ScannerPage implements OnInit {
   ngOnInit() {
   }
 
+  onCodeResult(result: string) {
+    this.scannedResult = result;
+  }
+
+  onError(error: any) {
+    console.error('Barcode scanning error:', error);
+  }
 }
